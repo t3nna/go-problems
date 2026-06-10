a90ca8a0a7b4ae602536cb3d53e9eb26c7dad417
Ivan Melnyk <t3nna1001@gmail.com>
2026-06-10 16:34:44 +0200

    tasks: repeat merge channels

--- 09-merge-channels/task.go
+++ 09-merge-channels/task.go
@@ -1,20 +1,23 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 func Merge(channels ...<-chan int) <-chan int {
 	res := make(chan int)
 
 	var wg sync.WaitGroup
 
-	wg.Add(len(channels))
 	for _, ch := range channels {
+		wg.Add(1)
 		go func(ch <-chan int) {
 			defer wg.Done()
 			for val := range ch {
+				fmt.Println(val)
 				res <- val
 			}
-
 		}(ch)
 	}
 
