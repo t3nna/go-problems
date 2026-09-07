fe80c3061376c7bf404d9bf31f96bb8254a889a8
Ivan Melnyk <t3nna1001@gmail.com>
2026-09-07 12:17:07 +0200

    tasks: solved

--- 19_snail/task.go
+++ 19_snail/task.go
@@ -5,17 +5,19 @@ import (
 )
 
 func main() {
-	snailMap := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	snailMap := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}                                // {1, 2, 3, 6, 9, 8, 7, 4, 5}
+	snailMap2 := [][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}} // {1, 2, 3, 6, 9, 8, 7, 4, 5}
 	fmt.Println(Snail(snailMap))
+	fmt.Println(Snail(snailMap2))
 }
 
 func Snail(snaipMap [][]int) []int {
 
 	dir := map[string][]int{
 		"r": {0, 1},
-		"b": {-1, 0},
+		"b": {1, 0},
 		"l": {0, -1},
-		"t": {1, 0},
+		"t": {-1, 0},
 	}
 
 	//   dirArr := [][]int{{0, 1}, {-1, 0}, {0, -1}, {1, 0}}
@@ -33,25 +35,30 @@ func Snail(snaipMap [][]int) []int {
 	place := [2]int{0, 0}
 	currDir := "r"
 	for true {
+		if visited[place[0]][place[1]] == true {
+			break
+		}
+
 		res = append(res, snaipMap[place[0]][place[1]])
 
-		newDir, isChanged := checkDir(place[0], place[1], dim, currDir, visited)
+		visited[place[0]][place[1]] = true
+
+		// make step
+		newDir, isChanged := checkDir(place[0]+dir[currDir][0], place[1]+dir[currDir][1], dim, currDir, visited)
 		if isChanged {
 			currDir = newDir
 		}
-		// make step
+
 		place[0] += dir[currDir][0]
 		place[1] += dir[currDir][1]
-		visited[place[0]][place[1]] = true
 
 		fmt.Println(visited)
 	}
 
-	return []int{}
+	return res
 }
 
 func checkDir(y, x, dim int, currDir string, visited [][]bool) (string, bool) {
-	dim--
 	if currDir == "r" && (x == dim || visited[y][x]) {
 		return "b", true
 	}
@@ -60,11 +67,11 @@ func checkDir(y, x, dim int, currDir string, visited [][]bool) (string, bool) {
 		return "l", true
 	}
 
-	if currDir == "l" && (x == 0 || visited[y][x]) {
+	if currDir == "l" && (x == -1 || visited[y][x]) {
 		return "t", true
 	}
 
-	if currDir == "t" && (y == 0 || visited[y][x]) {
+	if currDir == "t" && (y == -1 || visited[y][x]) {
 		return "r", true
 	}
 	return "", false
